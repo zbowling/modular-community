a075a589392bb48ad4d2c10a1d9badafcaf0583f
Julian Hofer <julianhofer@gnome.org>
2024-11-18 12:00:00 +0100

    Add schema

--- scripts/build-all.py
+++ scripts/build-all.py
@@ -3,18 +3,18 @@ from pathlib import Path
 import sys
 import json
 from datetime import datetime
-from typing import Any
 import argparse
+from .schemas import RecipeFailure
 
 
-def load_failed_compatibility(file_path: Path) -> dict[str, Any]:
+def load_failed_compatibility(file_path: Path) -> dict[str, RecipeFailure]:
     if file_path.exists():
         with file_path.open("r") as file:
             return dict(json.load(file))
     return {}
 
 
-def save_failed_compatibility(file_path: Path, data: dict[str, Any]) -> None:
+def save_failed_compatibility(file_path: Path, data: dict[str, RecipeFailure]) -> None:
     if not file_path.parent.exists():
         file_path.parent.mkdir(parents=True, exist_ok=True)
     with file_path.open("w") as file:
---
+++ scripts/schemas.py
@@ -0,0 +1,5 @@
+from typing import TypedDict
+
+
+class RecipeFailure(TypedDict):
+    failed_at: str
